b6d466e2a4737391487a21770c59a02a6be409d3
EvesProgram <evatsatsi18@gmail.com>
2026-07-22 02:32:47 +0200

    Buttons restored

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const cvFiles = {
 };
 
 const downloadCvBtn = document.getElementById('download-cv-btn');
+const viewCvBtn = document.getElementById('view-cv-btn');
 
 document.querySelectorAll('.cv-toggle-btn').forEach(btn => {
     btn.addEventListener('click', function() {
@@ -14,6 +15,7 @@ document.querySelectorAll('.cv-toggle-btn').forEach(btn => {
         const cv = cvFiles[this.getAttribute('data-cv')];
         downloadCvBtn.setAttribute('href', cv.path);
         downloadCvBtn.setAttribute('download', cv.filename);
+        viewCvBtn.setAttribute('href', cv.path);
     });
 });
 
